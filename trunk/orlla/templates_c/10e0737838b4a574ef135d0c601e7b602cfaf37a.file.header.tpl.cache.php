8cb268401bc407438749fd5c06941f6f3bf0218d
tecshuttle@gmail.com <tecshuttle@gmail.com@50d285e2-280e-b1ae-7fdc-db99dfcae3a2>
2012-10-24 01:11:18 +0000

    决定在新项目中使用Smarty模板引擎，加入相应文件。

---
+++ trunk/orlla/templates_c/10e0737838b4a574ef135d0c601e7b602cfaf37a.file.header.tpl.cache.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.12, created on 2012-10-24 01:09:19
+         compiled from ".\templates\header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:3035950873fbf1dd7c5-83331486%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '10e0737838b4a574ef135d0c601e7b602cfaf37a' => 
+    array (
+      0 => '.\\templates\\header.tpl',
+      1 => 1348576168,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '3035950873fbf1dd7c5-83331486',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+    'Name' => 1,
+  ),
+  'has_nocache_code' => true,
+  'version' => 'Smarty-3.1.12',
+  'unifunc' => 'content_50873fbf1ef677_46369096',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_50873fbf1ef677_46369096')) {function content_50873fbf1ef677_46369096($_smarty_tpl) {?><HTML>
+<HEAD>
+<TITLE><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+ - <?php echo '/*%%SmartyNocache:3035950873fbf1dd7c5-83331486%%*/<?php echo $_smarty_tpl->tpl_vars[\'Name\']->value;?>
+/*/%%SmartyNocache:3035950873fbf1dd7c5-83331486%%*/';?>
+</TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
+<?php }} ?>
