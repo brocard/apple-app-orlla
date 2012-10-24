84bbb1703c1cc0ae6ccef41c09626fd902f315ca
tecshuttle <tecshuttle@gmail.com>
2012-10-24 01:11:18 +0000

    决定在新项目中使用Smarty模板引擎，加入相应文件。

---
+++ orlla/templates_c/1be7ff7fdee636597edd726ee98dfef4bfd55d1f.file.footer.tpl.cache.php
@@ -0,0 +1,24 @@
+<?php /* Smarty version Smarty-3.1.12, created on 2012-10-24 01:09:19
+         compiled from ".\templates\footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1242050873fbf24e866-61052351%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '1be7ff7fdee636597edd726ee98dfef4bfd55d1f' => 
+    array (
+      0 => '.\\templates\\footer.tpl',
+      1 => 1348576168,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1242050873fbf24e866-61052351',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.12',
+  'unifunc' => 'content_50873fbf2555a9_48335215',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_50873fbf2555a9_48335215')) {function content_50873fbf2555a9_48335215($_smarty_tpl) {?></BODY>
+</HTML>
+<?php }} ?>
